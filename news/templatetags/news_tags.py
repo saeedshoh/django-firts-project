a71ac4e27fb51f410122d3c59234fd85178b935b
Saeedshoh <52253289+Saeedshoh@users.noreply.github.com>
2022-12-04 19:55:28 +0500

    frtbb

--- news/templatetags/news_tags.py
+++ news/templatetags/news_tags.py
@@ -1,9 +1,11 @@
 from django import template
 from news.models import Category
 
+from django.db.models import Count
+
 register = template.Library()
 
 
 @register.simple_tag()
 def get_categories():
-    return Category.objects.all()
+    return Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
--- news/urls.py
+++ news/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', HomeNews.as_view(), name='home'),
     path('category/<int:category_id>/', NewsBycategory.as_view(), name='category_show'),
     path('news/<int:news_id>/',GetPostView.as_view(), name='post_show'),
-    path('create-news', add_news, name='news.create')
+    path('create-news', add_news, name='news.create'),
+    path('tests', TestBaseView.as_view())
 ]
