4bde6ed119af403b6f92356d14eb3a9eb260a72d
Saeedshoh <52253289+Saeedshoh@users.noreply.github.com>
2022-12-15 21:16:08 +0500

    Многоуровневое меню. Часть 1

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
+    path('test/', include('testapp.urls')),
     path('', include('news.urls')),
 ]
 
